4cbd44d91b4af5ea4f92f90cb28b45a4ff34ee42
liji <lijiredback@163.com>
2019-08-29 01:13:20 +0800

    feat: Text Widget

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp()); // MyApp 是一个类
+void main() => runApp(MyApp()); // MyApp 是一个类
 
 class MyApp extends StatelessWidget {
   // build 方法
@@ -11,7 +11,18 @@ class MyApp extends StatelessWidget {
       title: 'Text Widget',
       home: Scaffold(
         body: Center(
-          child: Text('Hello Widget'),
+          child: Text(
+            'Hello Widget, 你好 Flutter, Hello Widget, 你好 Flutter, Hello Widget, 你好 Flutter, Hello Widget, 你好 Flutter, Hello Widget, 你好 Flutter, Hello Widget, 你好 Flutter,',
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 16,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid
+            ),
+          ),
         ),
       ),
     );
